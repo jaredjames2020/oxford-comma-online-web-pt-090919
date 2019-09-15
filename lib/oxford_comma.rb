81d9483b9d1c3979caa7c80c7d7c558fbb2c1ee8
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-09-15 15:03:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,13 @@
 require 'pry'
 
 def oxford_comma(array)
+  array2 = []
   if array.length == 1
     array.join
   elsif array.length == 2
     array.join(" and ")
+  else
+    array.join(", ")
      #binding.pry
   end
 end
